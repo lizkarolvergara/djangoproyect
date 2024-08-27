e428b48d52b4d70a0ad225a7e2fbac7fdd89da4a
Liz Karol Vergara <cva.k16@gmail.com>
2024-08-27 17:59:14 -0500

    Primer commit

---
+++ myapp/views.py
@@ -0,0 +1,12 @@
+
+from django.http import HttpResponse
+
+# Create your views here.
+
+def hello(request):
+    return HttpResponse("<h1>Hola petes</h1>")
+
+
+def about(request):
+    return HttpResponse('About')
+
