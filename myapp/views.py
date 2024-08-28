b32c43c6c8de86e5db664242ca100d42a3aa6cda
Liz Karol Vergara <cva.k16@gmail.com>
2024-08-28 16:00:09 -0500

    Segundo commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,22 @@
-
 from django.http import HttpResponse
+from .models import Project, Task
+from django.shortcuts import render
 
 # Create your views here.
-
-def hello(request):
-    return HttpResponse("<h1>Hola petes</h1>")
-
+def index(request):
+    return render(request, 'index.html')
 
 def about(request):
-    return HttpResponse('About')
+    return render(request, 'about.html')
 
+def hello(request, username):
+    print(username)
+    return HttpResponse("<h1>Hola %s</h1>" % username)
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return render(request, 'projects.html')
+
+def tasks(request):
+    # task = Task.objects.get(title=title)
+    return render(request, 'tasks.html')
