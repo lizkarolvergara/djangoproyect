66c965b13a9c5a57ccbc8f96cb8aa45922a57256
Liz Karol Vergara <cva.k16@gmail.com>
2024-09-02 10:32:43 -0500

    Tercer commit

--- myapp/views.py
+++ myapp/views.py
@@ -4,19 +4,31 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    title = 'django gaaaaa!!'
+    return render(request, 'index.html', {
+        'title': title
+    })
 
 def about(request):
-    return render(request, 'about.html')
+    username = "khiii"
+    return render(request, 'about.html', {
+        'username': username
+    })
 
 def hello(request, username):
     print(username)
     return HttpResponse("<h1>Hola %s</h1>" % username)
 
 def projects(request):
-    projects = list(Project.objects.values())
-    return render(request, 'projects.html')
+    # projects = list(Project.objects.values())
+    projects = Project.objects.all()
+    return render(request, 'projects.html', {
+        'projects': projects
+    })
 
 def tasks(request):
     # task = Task.objects.get(title=title)
-    return render(request, 'tasks.html')
+    tasks = Task.objects.all()
+    return render(request, 'tasks.html', {
+        'tasks': tasks
+    })
